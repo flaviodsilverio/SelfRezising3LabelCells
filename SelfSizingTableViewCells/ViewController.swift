3378649d523044cb098812af77ed913e4f719254
flaviodsilverio <flavio.silverio@headforwards.com>
2017-02-17 08:56:33 +0000

    Initial Commit

---
+++ SelfSizingTableViewCells/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SelfSizingTableViewCells
+//
+//  Created by Flávio Silvério on 17/02/17.
+//  Copyright © 2017 Flávio Silvério. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
